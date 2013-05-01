df4cf9d82cddf80ab375ca29a4de0e782b5f1d6a
Carwin Young <carwinyoung@gmail.com>
2013-05-01 00:16:58 -0500

    New test assertion added.

--- test/test_gemwin.rb
+++ test/test_gemwin.rb
@@ -4,13 +4,18 @@ require 'gemwin'
 
 class GemwinTest < Test::Unit::TestCase
   def test_english_hello
-    assert_equal "Konnichiwa bitches.",
+    assert_equal "Hello my fine friend.",
       Hello.in("english")
   end
 
   def test_japanese_hello
     assert_equal "こんにちはこのあま",
       Hello.in("japanese")
+  end
+
+  def test_wutang_hello
+    assert_equal "Konnichiwa bitches.",
+      Hello.in("wu-tang")
   end
 
   def test_any_hello
